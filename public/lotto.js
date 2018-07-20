6ec28a5905fb76196ad37bb87e957506d89928c6
MaciejReimann <maciej.reimann@gmail.com>
2018-07-20 13:34:44 +0200

    alerting validation condition statement in a mess

--- public/lotto.js
+++ public/lotto.js
@@ -1,4 +1,4 @@
-//var lottoMachine = new Worker ("worker.js");  
+//var lottoMachine = new Worker ("worker.js");
 var lottoMachine = new Worker(URL.createObjectURL(new Blob(["("+worker_function.toString()+")()"], {type: 'text/javascript'})));
 
 var workerMessage = {
@@ -66,20 +66,20 @@ var inputFieldSeconds = document.querySelector('#seconds');
 var launchButton = document.getElementById('launch-worker');
 var refreshButton = document.getElementById('refresh');
 
-//- - - - - - - - - Communication Functions- - - - - - - - - - - 
+//- - - - - - - - - Communication Functions- - - - - - - - - - -
 //--------------------------------------------------------------
 
-function replaceHTML(divIdName, content) {    
+function replaceHTML(divIdName, content) {
     document.getElementById(divIdName).innerHTML = content;
     }
 
-function placeToDiv(divIdName, outputToPrint) {    
+function placeToDiv(divIdName, outputToPrint) {
     document.getElementById(divIdName).innerHTML = outputToPrint;
     }
 function printAlert() {
     document.getElementById('validation-alerts').style.fontStyle = 'bold';
     placeToDiv('validation-alerts', alertValidation);
-    
+
 }
 function clearAlert() {
     alertValidation = '';
@@ -91,21 +91,21 @@ function clearAlert() {
 function getAllSiblings(elem, exclude) {
     var siblings = [];
     elem = elem.parentNode.firstChild;
-    do { 
+    do {
         if (elem.nodeType === 3) {continue}; //text node
-        if (elem !== exclude) siblings.push(elem)} 
+        if (elem !== exclude) siblings.push(elem)}
     while (elem = elem.nextSibling); //when elem.nextSibling is the last one it equals to null
-    return siblings; 
+    return siblings;
 }
 function goToNextSibling(elem) { //jumps from active element to next sibling element
     var next = elem.nextElementSibling;
     if ( next === null ) {return 'last' ; };// if it's the last element
-    return next.focus(); 
+    return next.focus();
 };
 function buttonOnOff (buttonId) {
-    if (document.getElementById(buttonId).disabled === false) 
+    if (document.getElementById(buttonId).disabled === false)
     {document.getElementById(buttonId).disabled = true;
-    } else if (document.getElementById(buttonId).disabled === true) 
+    } else if (document.getElementById(buttonId).disabled === true)
     {document.getElementById(buttonId).disabled = false};
 };
 
@@ -126,9 +126,9 @@ function numberOfTimesValidation (number) {
         return true;
     }
     numberOfTimesValidated = true;
-    return true;        
+    return true;
 }
-function checkIfValid (number) {   
+function checkIfValid (number) {
     var number = number;
     if (isNaN(parseInt(number))) {
         alertValidation = 'Come on! I need a number here.';
@@ -141,14 +141,14 @@ function checkIfValid (number) {
         return false;
     };
     for (var i=0; i<my6.length; i++) {
-        if (number === my6[i]) { 
+        if (number === my6[i]) {
             repeated++;
             };
         if (repeated > 1) {
             alertValidation = 'Didn\'t I say 6 UNIQUE numbers?';
             return false };
     };
-    
+
     return true;
 };
 function timerValidation (number) {
@@ -158,13 +158,13 @@ function timerValidation (number) {
         return false;
     } else if (number < 0) {
         alertValidation = 'Sorry, I was not allowed to do it'
-        return false; 
-    }  
+        return false;
+    }
     timerValidated = true;
     return true;
 };//returns false when NaN or < 0; else true
 
-//- - - - - - - - - - Timer Functions- - - - - - - - - - - - - 
+//- - - - - - - - - - Timer Functions- - - - - - - - - - - - -
 //-------------------------------------------------------------
 function timerToZero () {
     timer.seconds = 0;
@@ -196,31 +196,31 @@ function displayClock () {
 
 function clockRunning () {
     var interval = setInterval(function () {timeIt()}, 1000);
-    
+
     function timeIt() {
         secondsCounter++;
         if (workerFinished === true) {clearInterval(interval)};
-        displayClock();    
+        displayClock();
     };
 };
 function timerSetAndRunning() {
     updateTimeLeft();
-    
+
 var interval = setInterval(function () {timeIt()}, 1000);
     function timeIt() {
         secondsCounter++;
         if (secondsCounter === timeLeft) {clearInterval(interval)};
         displayTimer();
-    };    
+    };
 };//REVISION NEEDED -> We dont want it to display BLANK for the first second!!!
 function formatTimer(unit, seconds) {
     var unit = unit;
     var seconds = seconds;
     var hrs = Math.floor(seconds/3600);
     var min = Math.floor(seconds/60);
-    var sec = seconds % 60;        
+    var sec = seconds % 60;
     if (unit === 'sec') {return twoDigits(sec)};
-    if (unit === 'min') {return twoDigits(min)};   
+    if (unit === 'min') {return twoDigits(min)};
     if (unit === 'hrs') {return twoDigits(hrs)};
 };//returns sec/min/hrs
 function twoDigits(number) {
@@ -232,55 +232,55 @@ function twoDigits(number) {
 
 function sortMy6() {
         arr = [];
-    for (var i=0; i<my6.length; i++) {        
+    for (var i=0; i<my6.length; i++) {
         arr.push((parseInt(my6[i])));
         arr.sort(function(a, b){return a-b});
            }
         return my6sorted = arr;
         console.log(my6sorted);
-        
+
 };//returns User's sorted array
 function sortSave() {
     workerMessage.mySix = sortMy6();
     console.log('ok, sorted');
 //    placeToDiv('your-6', 'Your six numbers are: <br>'+workerMessage.mySix);
-};//writes User's sorted array to the 
+};//writes User's sorted array to the
 //workerMesssage object AND PLACE IT TO THE RIGHT;
 function arrayFiltered(array) {
     return array = array.filter(function(x) { return x !== undefined && x != null; } )
 };
 
-//- - - - - - - - -  BUTTONS & EVENT LISTENERS - - - - - - - - - - - - 
+//- - - - - - - - -  BUTTONS & EVENT LISTENERS - - - - - - - - - - - -
 
 function addChangeListeners (inputField, inputFieldIndex) {
     inputField.addEventListener("change", function() {
-        repeated = 0;  
+        repeated = 0;
         var num = inputField.value;
         var index = inputFieldIndex;
         var siblingFields = getAllSiblings(inputField, inputField);
-        
+
         checkIfValid(num);
-        
+
         if (checkIfValid(num) == false) {
             printAlert();; //prints an adequate message
             inputField.style.backgroundColor = 'pink'; //colors the field
             for (var key in siblingFields) {
                 siblingFields[key].disabled = true;
             } //disable all the other fields
-            inputField.focus(); //and holds the focus             
-        }; 
+            inputField.focus(); //and holds the focus
+        };
         if (checkIfValid(num) == true) {
             my6[inputFieldIndex] = num; // saves the input
             for (var key in siblingFields) {
                 siblingFields[key].disabled = false;
             };
-            inputField.style.border = 'solid 2px' 
+            inputField.style.border = 'solid 2px'
             inputField.style.backgroundColor = ''; // restores default
             goToNextSibling(inputField); //and goes to the next field
             clearAlert();
             buttonOnOff('refresh');
-            
-        
+
+
             if (goToNextSibling(inputField) ==='last'&&arrayFiltered(my6).length<6) {
                 // when last element has been filled CHECK OUT IF THE REST IS FILLED
                 numbersValidated = true // they have been validated one by one - so they are all validated by now
@@ -293,21 +293,21 @@ function addChangeListeners (inputField, inputFieldIndex) {
                         my6inputFields[i].focus(); //SHOWS WHICH FIELDS ARE EMPTY!!more than one!!!
                     };
                 };
-               
-            }; 
+
+            };
             if ( arrayFiltered(my6).length === 6) {
                 for (i=0; i<6; i++) {
                     if (checkIfValid(my6[i]) === true) {numbersValidated = true}
                 }
-                
+
                     alertValidation = 'OK. Thank you.';
                     printAlert();
-                    sortSave();                
+                    sortSave();
                     inputFieldHowManyTimes.focus();
             };
-        };        
-    }); 
-};  
+        };
+    });
+};
 for (i=0; i < my6inputFields.length; i++) {
     addChangeListeners(my6inputFields[i], i)
     } //LISTNERES TO 6 INPUT FIELDS
@@ -328,7 +328,7 @@ inputFieldHowManyTimes.addEventListener('change', function() {
         inputFieldHowManyTimes.style.backgroundColor = '';
         inputField1.focus()
     }
-    
+
     if(numberOfTimesValidated && numbersValidated) {
         workerMessage.HowManyTimes = inputFieldHowManyTimes.value;
         document.querySelector('#hours').disabled = true;
@@ -337,7 +337,7 @@ inputFieldHowManyTimes.addEventListener('change', function() {
         inputFieldHowManyTimes.style.border = 'solid 2px';
         inputFieldHowManyTimes.style.backgroundColor = '';
         buttonOnOff('launch-worker');
-        launchButton.focus()};    
+        launchButton.focus()};
     if (workerMessage.HowManyTimes === 0) {
         document.querySelector('#hours').disabled = false;
         document.querySelector('#minutes').disabled = false;
@@ -354,8 +354,8 @@ inputFieldSeconds.addEventListener('change', function() {
     var number = inputFieldSeconds.value;
     clearAlert();
     timerValidation(number);
-    if (!timerValidated) {printAlert(); inputFieldMinutes.focus();} 
-    else if (timerValidated && !numbersValidated) {inputField1.focus()} 
+    if (!timerValidated) {printAlert(); inputFieldMinutes.focus();}
+    else if (timerValidated && !numbersValidated) {inputField1.focus()}
     else if (timerValidated) {
             timer.seconds = parseInt(number);//sets value to the timer Object
             updateTimeLeft();
@@ -367,9 +367,9 @@ inputFieldSeconds.addEventListener('change', function() {
 inputFieldMinutes.addEventListener('change', function() {
     var number = inputFieldMinutes.value;
     clearAlert();
-    timerValidation(number) 
-    if (!timerValidated) {printAlert(); inputFieldMinutes.focus();} 
-    else if (timerValidated && !numbersValidated) {inputField1.focus()} 
+    timerValidation(number)
+    if (!timerValidated) {printAlert(); inputFieldMinutes.focus();}
+    else if (timerValidated && !numbersValidated) {inputField1.focus()}
     else if (timerValidated) {
             timer.minutes = parseInt(number);//sets value to the timer Object
             updateTimeLeft();
@@ -381,9 +381,9 @@ inputFieldMinutes.addEventListener('change', function() {
 inputFieldHours.addEventListener('change', function() {
     var number = inputFieldHours.value;
     clearAlert();
-    timerValidation(number) 
-    if (!timerValidated) {printAlert(); inputFieldMinutes.focus();} 
-    else if (timerValidated && !numbersValidated) {inputField1.focus()} 
+    timerValidation(number)
+    if (!timerValidated) {printAlert(); inputFieldMinutes.focus();}
+    else if (timerValidated && !numbersValidated) {inputField1.focus()}
     else if (timerValidated) {
             timer.minutes = parseInt(number);//sets value to the timer Object
             updateTimeLeft();
@@ -395,25 +395,25 @@ inputFieldHours.addEventListener('change', function() {
 //-----
 
 refreshButton.addEventListener("click", function () {
-    
+
     window.location.reload();
 })
 launchButton.addEventListener("click", function() {
-    console.log('LAUNCHED!');    
+    console.log('LAUNCHED!');
     lottoMachine.postMessage(workerMessage);
     replaceHTML('welcome', text_computing);
     buttonOnOff('launch-worker');
     buttonOnOff('refresh');
     if(timeLeft == 0) {
-        replaceHTML('timer-elements', timerLaunched);    
+        replaceHTML('timer-elements', timerLaunched);
         elementsTurnedHidden ();
         clockRunning()
         console.log('this')
     };
     if (timeLeft > 0) {
-        replaceHTML('timer-elements', timerLaunched);    
+        replaceHTML('timer-elements', timerLaunched);
         elementsTurnedHidden ();
-        timerSetAndRunning();       
+        timerSetAndRunning();
     };
 }); // sends the message to the worker & turns on refresh button
 
@@ -428,7 +428,7 @@ lottoMachine.addEventListener("message", function(event) {
     if(parseInt(workerFound)>0 && parseInt(workerFound)<1) {
         alertValidation = 'I found ' + workerFound + ' match in ' + workerCounter + ' draws, <br>which gives an average of one in' +chanceOfSix+'draws.'
     };
-    alertValidation = 'I found ' + workerFound + ' matches in ' + workerCounter + ' draws.'; 
+    alertValidation = 'I found ' + workerFound + ' matches in ' + workerCounter + ' draws.';
     printAlert();
 //    printReport();
     if (workerFinished) {replaceHTML('welcome', text_finished)};
@@ -439,6 +439,6 @@ lottoMachine.addEventListener("message", function(event) {
 
 //function printReport(data) {
 //    data = '<li>' + workerFound + secondsCounter + '</li>'
-//   
+//
 //    placeToDiv('report', data);
 //}
